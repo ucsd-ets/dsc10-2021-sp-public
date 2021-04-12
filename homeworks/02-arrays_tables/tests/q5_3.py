52b783f18a2634efc737d399f0b6d224d2a46b64
nixrunner <nixrunner@localhost>
2021-04-12 01:41:42 +0000

    Machine user push.

---
+++ homeworks/02-arrays_tables/tests/q5_3.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q5_3',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> import numbers;\n>>> isinstance(first_day_of_class, bpd.DataFrame)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
