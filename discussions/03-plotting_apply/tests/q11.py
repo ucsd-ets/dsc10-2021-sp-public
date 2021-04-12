836ae55e3b532e32ab56564499fd282d0feb4b02
nixrunner <nixrunner@localhost>
2021-04-12 20:45:34 +0000

    Machine user push.

---
+++ discussions/03-plotting_apply/tests/q11.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q11',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> count_is_increasing == True\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ discussions/03-plotting_apply/tests/q12.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q12',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> median_is_increasing == False\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
