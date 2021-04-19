00f399b21934eb914ed76c89ae1c4a37fc691f92
nixrunner <nixrunner@localhost>
2021-04-19 21:36:41 +0000

    Machine user push.

---
+++ discussions/04-simulation/tests/q15.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q15',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': ">>> college_size(100) == 'tiny' and college_size(5000) == 'small' and college_size(15000) == 'medium' and college_size(20000) == 'large'\nTrue",
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
