97fe63fe2179ed6977f6e2405d051f0cffd4d094
nixrunner <nixrunner@localhost>
2021-05-10 02:46:49 +0000

    Machine user push.

---
+++ homeworks/05-hypothesis/tests/q23.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q23',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> isinstance(unreasonable_test_statistics, list)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
